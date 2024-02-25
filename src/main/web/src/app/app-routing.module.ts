ed946ff3262cd07dfacecc8cc2447f8ed7bac5cb
Tanguy OZANO <tanguy.ozano@gmail.com>
2024-02-25 17:53:14 +0100

    ✨ Made homepage example

--- src/main/web/src/app/app-routing.module.ts
+++ src/main/web/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from "./profile/profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'connection', component: ConnectionComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/web/src/app/app.module.ts
+++ src/main/web/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ConnectionComponent } from './connection/connection.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    ConnectionComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
